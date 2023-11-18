4b157d69e9be488a2be9c487c4ce7946f75f74b9
volesen <vincolesen@gmail.com>
2023-11-18 19:17:09 +0100

    Added testing infrastructure

---
+++ test/hugorm.ml
@@ -0,0 +1,29 @@
+open Hugorm
+open Hugorm.Asm
+open Hugorm.Syntax
+
+let asm =
+  let pp_asm fmt asm = Format.fprintf fmt "%s" (Asm.string_of_asm asm) in
+  Alcotest.testable pp_asm ( = )
+
+let test_compile_num () =
+  Alcotest.(check asm)
+    "Compile `Num`"
+    [ IMov (Reg RAX, Const 42L) ]
+    (Main.compile_expr (Num 42L))
+
+let test_compile_add1 () =
+  Alcotest.(check asm)
+    "Compile `Add1`"
+    [ IMov (Reg RAX, Const 42L); IAdd (Reg RAX, Const 1L) ]
+    (Main.compile_expr (Add1 (Num 42L)))
+
+let () =
+  Alcotest.run "Compile"
+    [
+      ( "Compile expr",
+        [
+          Alcotest.test_case "Compile `Num`" `Quick test_compile_num;
+          Alcotest.test_case "Compile `Add1`" `Quick test_compile_add1;
+        ] );
+    ]
