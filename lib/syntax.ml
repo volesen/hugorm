c39ad993890844968823169d5400814b89e2d1a3
volesen <vincolesen@gmail.com>
2023-11-19 09:25:08 +0100

    Added interpretation of binary ops

--- lib/syntax.ml
+++ lib/syntax.ml
@@ -3,8 +3,10 @@ type 'a program = 'a expr
 and 'a expr =
   | ENumber of int64 * 'a
   | EPrim1 of prim1 * 'a expr * 'a
+  | EPrim2 of prim2 * 'a expr * 'a expr * 'a
   | EId of string * 'a
   | ELet of string * 'a expr * 'a expr * 'a
   | EIf of 'a expr * 'a expr * 'a expr * 'a
 
 and prim1 = Add1 | Sub1
+and prim2 = Add | Sub | Mul
