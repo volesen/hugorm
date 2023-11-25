ad2db780b52a378c2d117959de96bfe522288951
volesen <vincolesen@gmail.com>
2023-11-25 20:04:00 +0100

    Extended AST with bools and boolean ops

--- lib/syntax.ml
+++ lib/syntax.ml
@@ -2,11 +2,24 @@ type 'a program = 'a expr
 
 and 'a expr =
   | ENumber of int64 * 'a
+  | EBool of bool * 'a
   | EPrim1 of prim1 * 'a expr * 'a
   | EPrim2 of prim2 * 'a expr * 'a expr * 'a
   | EId of string * 'a
   | ELet of string * 'a expr * 'a expr * 'a
   | EIf of 'a expr * 'a expr * 'a expr * 'a
 
-and prim1 = Neg
-and prim2 = Add | Sub | Mul
+and prim1 = Neg | Not
+
+and prim2 =
+  | Add
+  | Sub
+  | Mul
+  | And
+  | Or
+  | Less
+  | Greater
+  | LessEq
+  | GreaterEq
+  | Eq
+  | Ne
