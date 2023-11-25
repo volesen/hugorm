ede4c55c6c01086db64c8019bc8bc44d54be70e0
volesen <vincolesen@gmail.com>
2023-11-25 20:22:51 +0100

    Added short-circuiting

--- lib/eval.ml
+++ lib/eval.ml
@@ -4,6 +4,12 @@ module Env = Map.Make (String)
 type value = VInt of int64 | VBool of bool
 and env = value Env.t
 
+(* Tagged values *)
+let min_int = Int64.div Int64.min_int 2L
+let max_int = Int64.div Int64.max_int 2L
+let const_true = 0L
+let const_false = 0L
+
 let string_of_value v =
   match v with VInt n -> Int64.to_string n | VBool b -> string_of_bool b
 
@@ -15,6 +21,7 @@ let rec eval (env : env) (e : 'a expr) : value =
   | ENumber (n, _) -> VInt n
   | EBool (b, _) -> VBool b
   | EPrim1 (op, e, _) -> eval_prim1 env op e
+  | EPrim2 (((And | Or) as op), l, r, _) -> eval_prim2' env op l r
   | EPrim2 (op, l, r, _) -> eval_prim2 env op l r
   | EId (x, _) -> eval_id env x
   | ELet (x, e1, e2, _) -> eval_let env x e1 e2
@@ -44,13 +51,21 @@ and eval_prim2 env op l r =
   | Add, VInt a, VInt b -> VInt (Int64.add a b)
   | Sub, VInt a, VInt b -> VInt (Int64.sub a b)
   | Mul, VInt a, VInt b -> VInt (Int64.mul a b)
-  (* TODO: Short circuit `And` and `Or` *)
-  | And, VBool a, VBool b -> VBool (a && b)
-  | Or, VBool a, VBool b -> VBool (a && b) 
   | Less, VInt a, VInt b -> VBool (a < b)
   | Greater, VInt a, VInt b -> VBool (a > b)
   | LessEq, VInt a, VInt b -> VBool (a <= b)
   | GreaterEq, VInt a, VInt b -> VBool (a >= b)
   | Eq, VInt a, VInt b -> VBool (a = b)
   | Ne, VInt a, VInt b -> VBool (a <> b)
+  | _ -> failwith err_type_mismatch
+
+and eval_prim2' env op l r =
+  let assert_bool v =
+    match v with VBool _ as v -> v | _ -> failwith err_type_mismatch
+  in
+  match (op, eval env l) with
+  | And, VBool true -> assert_bool (eval env r)
+  | And, VBool false -> VBool false
+  | Or, VBool true -> VBool true
+  | Or, VBool false -> assert_bool (eval env r)
   | _ -> failwith err_type_mismatch
