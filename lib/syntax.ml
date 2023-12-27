26205dd19d635d99858c44102bcc9f1c7644692a
Vincent Olesen <volesen@users.noreply.github.com>
2023-12-27 20:40:54 +0100

    Refactor to separate ANF type (#13)
    * Refactored to typed ANF
    
    * Move tag into `Syntax` module

--- lib/syntax.ml
+++ lib/syntax.ml
@@ -1,8 +1,5 @@
-type 'a program = { decls : 'a decl list; body : 'a expr }
-[@@deriving show]
-
-and 'a decl = DFun of string * string list * 'a expr * 'a
-[@@deriving show]
+type 'a program = { decls : 'a decl list; body : 'a expr } [@@deriving show]
+and 'a decl = DFun of string * string list * 'a expr * 'a [@@deriving show]
 
 and 'a expr =
   | ENumber of int64 * 'a
@@ -13,11 +10,9 @@ and 'a expr =
   | ELet of string * 'a expr * 'a expr * 'a
   | EIf of 'a expr * 'a expr * 'a expr * 'a
   | EApp of string * 'a expr list * 'a
-  [@@deriving show]
-
-and prim1 = Neg | Not
 [@@deriving show]
 
+and prim1 = Neg | Not [@@deriving show]
 
 and prim2 =
   | Add
@@ -31,8 +26,75 @@ and prim2 =
   | GreaterEq
   | Eq
   | Ne
-  [@@deriving show]
-
-
-type tag = int
 [@@deriving show]
+
+type tag = int [@@deriving show]
+
+let tag_of_expr (e : 'a expr) : 'a =
+  match e with
+  | ENumber (_, tag) -> tag
+  | EBool (_, tag) -> tag
+  | EPrim1 (_, _, tag) -> tag
+  | EPrim2 (_, _, _, tag) -> tag
+  | EId (_, tag) -> tag
+  | ELet (_, _, _, tag) -> tag
+  | EIf (_, _, _, tag) -> tag
+  | EApp (_, _, tag) -> tag
+
+let rec tag_program (prog : 'a program) : tag program =
+  let decls, tag = tag_decls prog.decls 0 in
+  let body, _ = tag_expr prog.body tag in
+  { decls; body }
+
+and tag_decls decls tag =
+  let decls, tag =
+    List.fold_left
+      (fun (decls, tag) decl ->
+        let decl, tag = tag_decl decl tag in
+        (decl :: decls, tag))
+      ([], tag) decls
+  in
+  (List.rev decls, tag)
+
+and tag_decl d tag =
+  match d with
+  | DFun (name, args, body, _) ->
+      let body, tag = tag_expr body tag in
+      (DFun (name, args, body, tag), tag + 1)
+
+and tag_expr e tag =
+  match e with
+  | ENumber (n, _) -> (ENumber (n, tag), tag + 1)
+  | EBool (b, _) -> (EBool (b, tag), tag + 1)
+  | EPrim1 (op, e, _) ->
+      let e, tag = tag_expr e tag in
+      (EPrim1 (op, e, tag), tag + 1)
+  | EPrim2 (op, e1, e2, _) ->
+      let e1, tag = tag_expr e1 tag in
+      let e2, tag = tag_expr e2 tag in
+      (EPrim2 (op, e1, e2, tag), tag + 1)
+  | EId (x, _) -> (EId (x, tag), tag + 1)
+  | ELet (x, e1, e2, _) ->
+      let e1, tag = tag_expr e1 tag in
+      let e2, tag = tag_expr e2 tag in
+      (ELet (x, e1, e2, tag), tag + 1)
+  | EIf (cond, thn, els, _) ->
+      let cond, tag = tag_expr cond tag in
+      let thn, tag = tag_expr thn tag in
+      let els, tag = tag_expr els tag in
+      (EIf (cond, thn, els, tag), tag + 1)
+  | EApp (name, args, _) ->
+      let args, tag = tag_args args tag in
+      (EApp (name, args, tag), tag + 1)
+
+and tag_args args tag =
+  let args, tag =
+    List.fold_left
+      (fun (args, tag) arg ->
+        let arg, tag = tag_expr arg tag in
+        (arg :: args, tag))
+      ([], tag) args
+  in
+  (List.rev args, tag)
+
+let tag p = tag_program p
